d680ba1a2b7ac605f32514616df275f8498e08d2
JonasGomes <jonasgoms@hotmail.com>
2019-04-28 22:04:24 -0300

    Adicionado o Swagger no Projeto

---
+++ src/main/java/org/locadora/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+/*********************************************************************************************************************
+ *
+ * COPYRIGHT 2017 TETRABIT DO BRASIL
+ *
+ * Terms of Use: This source code is licensed under the
+ *
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) license,
+ *
+ * where a full copy of these terms follow this work in the LICENSE file and is also available at:
+ *
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/
+ *
+ * You may not use this source code except in compliance with this License.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ ********************************************************************************************************************/
+package org.locadora.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api(){
+    return new Docket(DocumentationType.SWAGGER_2)
+               .select()
+               .apis(RequestHandlerSelectors.any())
+               .paths(PathSelectors.any())
+               .build();
+  }
+}
