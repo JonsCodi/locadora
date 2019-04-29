0fc038251a5e21726975a4eb152583aa66722fde
JonasGomes <jonasgoms@hotmail.com>
2019-04-29 01:39:46 -0300

    Removendo o Copy Right das classes...

--- src/main/java/org/locadora/resources/ClientResource.java
+++ src/main/java/org/locadora/resources/ClientResource.java
@@ -1,21 +1,4 @@
-/*********************************************************************************************************************
- *
- * COPYRIGHT 2017 TETRABIT DO BRASIL
- *
- * Terms of Use: This source code is licensed under the
- *
- * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) license,
- *
- * where a full copy of these terms follow this work in the LICENSE file and is also available at:
- *
- * https://creativecommons.org/licenses/by-nc-nd/4.0/
- *
- * You may not use this source code except in compliance with this License.
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- ********************************************************************************************************************/
+
 package org.locadora.resources;
 
 import io.swagger.annotations.Api;
